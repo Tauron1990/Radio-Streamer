64e10896b11ef6c74304db300a2f39dff8ac6dec
Tauron1990 <Game-over-Alexander@web.de>
2014-01-21 20:50:08 +0100

    initial

---
+++ ConsoleApplication/bin/Debug/Scripts/Technobase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Tauron.Application.RadioStreamer.Database.Scripts.CSharp;
+using Un4seen.Bass;
+using Un4seen.Bass.AddOn.Tags;
+
+namespace Tauron.Scripts
+{
+    public class Technobase : ScriptObject
+    {
+        protected override object Invoke()
+        {
+            string title;
+
+            try
+            {
+                var contentShow = Document.GetElementbyId("Content_Show");
+                var trackContent = contentShow.ChildNodes[3];
+
+                title = trackContent.ChildNodes.Count > 2
+                            ? trackContent.ChildNodes[1].InnerText
+                            : trackContent.InnerText;
+
+                title.Trim();
+            }
+            catch
+            {
+                title = "Unkown";
+            }
+            string[] names = title.Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+
+            string artist;
+
+            if (names.Length == 2)
+            {
+                artist = names[0].Trim();
+                title = names[1].Trim();
+            }
+            else
+            {
+                artist = title;
+                title = String.Empty;
+            }
+
+            return new TAG_INFO
+            {
+                tagType = BASSTag.BASS_TAG_ID3V2,
+                title = title,
+                artist = artist,
+                bitrate = 128,
+                albumartist = artist,
+                filename = title
+            };
+        }
+    }
+}
